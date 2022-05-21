bbcb5277477da5c442d6eb948b57429e823bfa36
Coducteur <96320345+Coducteur@users.noreply.github.com>
2022-05-21 18:24:38 +0200

    adding mainNAV

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// message to show when sending form:
 const message = "thx for your message, we will contact you ASAP !";
 
+// "ecoute" la partie formulaire et affiche le message en popup quand l'evenement "submit" est enclenché (l'utlisateur a cliqué sur le bouton submit)
 document
   .getElementById("formContact")
   .addEventListener("submit", function (event) {
